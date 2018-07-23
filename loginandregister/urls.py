9b421c285407a7cb3009d63f2278ecdaacd043a7
sharif485 <sharifahamed485@gmail.com>
2018-07-23 15:16:36 +0600

    Committed after login added

--- loginandregister/urls.py
+++ loginandregister/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import login
 
 urlpatterns = [
-    path('', views.loginandregister),
+    path('', login , {'template_name':'loginandregister/loginandregister.html'}),
+    path('profile/', views.profile),
 ]
--- loginandregister/views.py
+++ loginandregister/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
+from django.http import *
+from django.shortcuts import render_to_response,redirect
+from django.template import RequestContext
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 def loginandregister(request):
     return render(request, 'loginandregister/loginandregister.html')
+
+
+
+def login_user(request):
+
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('profile/')
+    return render_to_response('loginandregister.html', context_instance=RequestContext(request))
+
+@login_required(login_url='/profile/')
+def profile(request):
+    return render(request, 'loginandregister/profile.html')
